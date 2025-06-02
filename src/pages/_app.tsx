82d208fbb325f576b0ed648c1e6b822a39d48c7e
0xMatt <matthew.javelet@gmail.com>
2025-06-02 12:21:11 -0500

    update app/document structure to allow for new theme switching component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,21 @@
 import type { AppProps } from 'next/app';
-import "@/styles/global.css";
-
+import { ThemeProvider } from 'next-themes';
+import Layout from '@/components/layouts';
+import '@/styles/global.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
-}
+    return (
+        <>
+            <ThemeProvider
+                attribute="class"
+                defaultTheme="system"
+                enableSystem
+                disableTransitionOnChange
+            >
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+            </ThemeProvider>
+        </>
+    );
+};
