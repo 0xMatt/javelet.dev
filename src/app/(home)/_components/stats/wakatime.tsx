f908e1385705cd27830c2ccab7c11692def1fb34
0xMatt <matthew.javelet@gmail.com>
2025-06-12 16:38:47 -0500

    Redundancy cleanup: fetcher moved to lib, home stats plcaeholder

--- src/app/(home)/_components/stats/wakatime.tsx
+++ src/app/(home)/_components/stats/wakatime.tsx
@@ -4,41 +4,20 @@ import {Card, CardDescription, CardHeader, CardTitle} from "@/components/ui/card
 import {Clock, Hash} from "lucide-react";
 import {Badge} from "@/components/ui/badge";
 import {PlaceholderPattern} from "@/components/elements/placeholder-pattern";
-import {Skeleton} from "@/components/ui/skeleton";
 import useSWR from "swr";
-
-// @ts-expect-error any type is allowed
-const fetcher = (...args) => fetch(...args).then((res) => res.json())
+import StatSkeleton from "@/app/(home)/_components/stats/skeleton";
+import {fetcher} from "@/lib/fetcher";
 
 export default function WakaTime() {
 
-
     const {data} = useSWR(`/api/services/wakatime`, fetcher);
-
-    if (!data) return (
-        <Card
-            className="@container/card relative border-sidebar-border/90 dark:border-sidebar-border hover:scale-101 animate-pulse">
-            <PlaceholderPattern
-                className="absolute inset-0 size-full stroke-neutral-300/20 dark:stroke-neutral-100/20"/>
-            <CardHeader>
-                <CardDescription className={"flex flex-1"}>
-                    <Skeleton className="h-[20px] w-25 rounded-xl "/>
-                </CardDescription>
-                <CardTitle className="text-lg">
-                    <Skeleton className="h-[30px] w-75 rounded-xl "/>
-                </CardTitle>
-                <div className="line-clamp-1 flex gap-2 font-medium">
-                    <Skeleton className="h-[20px] w-20 rounded-xl "/> <Skeleton className="h-[20px] w-20 rounded-xl "/>
-                </div>
-            </CardHeader>
-        </Card>
-    )
-
+    if (!data) return (<StatSkeleton/>)
 
     const tags: string[] = ['programming', 'daily average'];
 
     return (
-        <Card className="@container/card relative border-sidebar-border/90 dark:border-sidebar-border hover:scale-101">
+        <Card
+            className="@container/card relative border-sidebar-border/90 dark:border-sidebar-border hover:scale-101 h-[130px]">
             <PlaceholderPattern
                 className="absolute inset-0 size-full stroke-neutral-300/20 dark:stroke-neutral-100/20"/>
             <CardHeader>
