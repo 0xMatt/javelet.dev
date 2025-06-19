86a5fe14d361506c168866e95aeec81b5fc440bc
0xMatt <matthew.javelet@gmail.com>
2025-06-18 21:22:12 -0500

    Larger width for mobile on HeaderTop. fixes #9

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,3 +1,3 @@
 export default function BlogLayout({children}: { children: React.ReactNode }) {
-    return <div className="mx-auto w-9/12 py-4">{children}</div>
+    return <div className="mx-auto w-11/12 lg:w-9/12 py-4">{children}</div>
 }
