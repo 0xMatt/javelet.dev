51c5766593b160d02797ac0049b47e21ada6a1e5
0xMatt <matthew.javelet@gmail.com>
2025-06-25 18:20:32 -0500

    Dynamically add blog posts to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,13 +1,32 @@
-import { MetadataRoute } from 'next';
-import { MENU_ITEMS } from '@/constants/menu';
+import {MetadataRoute} from 'next';
+import {MENU_ITEMS} from '@/constants/menu';
+import prisma from "@/services/prisma";
+
+let posts: Array<{
+    url: string;
+    lastModified: Date;
+    changeFrequency: "weekly" | "always" | "hourly" | "daily" | "monthly" | "yearly" | "never" | undefined;
+    priority: number;
+}> = [];
+
+const data =  await prisma.post.findMany();
+posts = data.map(post => {
+    console.log('post', post);
+    return {
+        url: process.env.APP_URL + '/blog/' + post.slug,
+        lastModified: post.createdAt,
+        changeFrequency: 'weekly',
+        priority: 0.9
+    }
+});
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return MENU_ITEMS.map((item) => {
-    return {
-      url: process.env.APP_URL + item.url,
-      lastModified: new Date(),
-      changeFrequency: item.changeFrequency,
-      priority: item.priority,
-    };
-  });
+    return MENU_ITEMS.map((item) => {
+        return {
+            url: process.env.APP_URL + item.url,
+            lastModified: new Date(),
+            changeFrequency: item.changeFrequency,
+            priority: item.priority,
+        };
+    }).concat(posts);
 }
