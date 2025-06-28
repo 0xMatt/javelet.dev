c793c0ea1e6f3a31424b325d2c8c2b3120a43cb3
0xMatt <matthew.javelet@gmail.com>
2025-06-27 22:40:15 -0500

    Fix up /uses so I can add to their repository

--- src/app/uses/_components/hardware.tsx
+++ src/app/uses/_components/hardware.tsx
@@ -1,8 +1,6 @@
 'use client';
 
 import { AnimatePresence, motion } from 'framer-motion';
-import { Button } from '@/components/ui/button';
-import { ExternalLink, ThumbsUp } from 'lucide-react';
 import Image, { StaticImageData } from 'next/image';
 import Link from 'next/link';
 
@@ -26,7 +24,35 @@ export interface HardwareItem {
   description: string;
   url: string;
 }
-export const Hardware = ({ hardwares }: { hardwares: HardwareItem[] }) => (
+
+const hardware: HardwareItem[] = [
+  {
+    name: 'HP OMEN - 16.1"',
+    description: 'Intel Core i7 - 16GB Memory - NVIDIA GeForce RTX 3060 - 1TB SSD',
+    image: 'https://i.imgur.com/OlaljAY.png',
+    url: 'https://www.bestbuy.com/site/hp-omen-16-2k-144hz-gaming-laptop-amd-ryzen-9-8940hx-32gb-ddr5-memory-nvidia-geforce-rtx-5060-1tb-ssd-shadow-black',
+  },
+  {
+    name: 'LG - UltraGear 32”',
+    description: 'Nano IPS QHD 1-ms G-SYNC with HDR',
+    image: 'https://i.imgur.com/Xwisfdv.png',
+    url: 'https://www.lg.com/us/monitors/lg-32gp83b-b-gaming-monitor',
+  },
+  {
+    name: 'Ducky Mecha SF Pro',
+    description: 'Silent Cherry Reds',
+    image: 'https://i.imgur.com/XS6dxEh.png',
+    url: 'https://mechanicalkeyboards.com/products/ducky-mecha-sf-pro-black',
+  },
+  {
+    name: 'G Pro X Superlight 2',
+    description: '4k Lightweight mouse',
+    image: 'https://i.imgur.com/weExwTV.png',
+    url: 'https://www.logitechg.com/en-us/products/gaming-mice/pro-x2-superlight-wireless-mouse.910-006628.html',
+  },
+];
+
+export const Hardware = () => (
   <AnimatePresence>
     <motion.div
       className="space-y-4"
@@ -35,38 +61,30 @@ export const Hardware = ({ hardwares }: { hardwares: HardwareItem[] }) => (
       exit="hidden"
       variants={listVariants}
     >
-      <div className="grid w-full grid-cols-2 gap-3">
-        {hardwares.map((hardware, index: number) => (
+      <div className="text-lg">Hardware</div>
+      <div className="grid w-full grid-cols-1 gap-3 pb-10 lg:grid-cols-2">
+        {hardware.map((hardware, index: number) => (
           <motion.div
             key={index}
             className="flex items-center justify-between gap-2"
             variants={itemVariants}
             transition={{ type: 'tween' }}
           >
-            <div className="flex items-center gap-4">
-              {/*<Image src={hardware.image} width={100} height={100} alt={hardware.name} />*/}
-              <Image
-                src={hardware.image}
-                width={80}
-                height={80}
-                alt={`${hardware.name} - ${hardware.description}`}
-              />
-              <div>
-                <span className="block text-sm leading-none font-semibold">{hardware.name}</span>
-                <span className="text-xs leading-none">{hardware.description}</span>
+            <Link href={hardware.url} target="_blank">
+              <div className="flex items-center gap-4">
+                {/*<Image src={hardware.image} width={100} height={100} alt={hardware.name} />*/}
+                <Image
+                  src={hardware.image}
+                  width={80}
+                  height={80}
+                  alt={`${hardware.name} - ${hardware.description}`}
+                />
+                <div>
+                  <span className="block text-sm leading-none font-semibold">{hardware.name}</span>
+                  <span className="text-xs leading-none">{hardware.description}</span>
+                </div>
               </div>
-            </div>
-            <div className="flex items-center gap-2">
-              <Link href={hardware.url} target="_blank">
-                <Button size="icon" variant="outline" className="cursor-pointer">
-                  <ExternalLink className="h-5 w-5" />
-                </Button>
-              </Link>
-
-              <Button size="icon" variant="outline" className="text-orange-300">
-                <ThumbsUp className="h-5 w-5" />
-              </Button>
-            </div>
+            </Link>
           </motion.div>
         ))}
       </div>
--- src/app/uses/page.tsx
+++ src/app/uses/page.tsx
@@ -1,7 +1,8 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { Hardware, HardwareItem } from '@/app/uses/_components/hardware';
+import { Hardware } from '@/app/uses/_components/hardware';
 import { Metadata } from 'next';
 import PageHeader from '@/components/elements/page-header';
+import { Software } from '@/app/uses/_components/software';
 
 export const metadata: Metadata = {
   title: 'My Uses',
@@ -9,57 +10,32 @@ export const metadata: Metadata = {
     "My uses, basically an alternative to an EDC showcase. Make sure to check out uses.tech for a list of everyone's /uses pages!",
 };
 
-const hardware: HardwareItem[] = [
-  {
-    name: 'HP OMEN - 16.1"',
-    description: 'Intel Core i7 - 16GB Memory - NVIDIA GeForce RTX 3060 - 1TB SSD',
-    image: 'https://i.imgur.com/OlaljAY.png',
-    url: 'https://www.bestbuy.com/site/hp-omen-16-2k-144hz-gaming-laptop-amd-ryzen-9-8940hx-32gb-ddr5-memory-nvidia-geforce-rtx-5060-1tb-ssd-shadow-black',
-  },
-  {
-    name: 'LG - UltraGear 32”',
-    description: 'Nano IPS QHD 1-ms G-SYNC with HDR',
-    image: 'https://i.imgur.com/Xwisfdv.png',
-    url: 'https://www.lg.com/us/monitors/lg-32gp83b-b-gaming-monitor',
-  },
-  {
-    name: 'Ducky Mecha SF Pro',
-    description: 'Silent Cherry Reds',
-    image: 'https://i.imgur.com/XS6dxEh.png',
-    url: 'https://mechanicalkeyboards.com/products/ducky-mecha-sf-pro-black',
-  },
-];
-
 export default function Page() {
   return (
     <>
       <PageHeader header={metadata.title as string} description={metadata.description} />
+      <img src="https://i.imgur.com/DtFWNAO.png" alt="My desktop" width={900} height={500}/>
+      <p>I use JetBrains IDE's for development. I am currently experimenting with their new oneisland theme and happy with experience.</p>
       <Tabs defaultValue="all" className="w-full">
-        <TabsList className="grid w-full grid-cols-5">
+        <TabsList className="grid w-full grid-cols-3 mb-15 lg:mb-0">
           <TabsTrigger value="all">All</TabsTrigger>
           <TabsTrigger value="hardware">Hardware</TabsTrigger>
           <TabsTrigger value="software">Software</TabsTrigger>
-          <TabsTrigger value="coding">Browser & Coding</TabsTrigger>
-          <TabsTrigger value="hobby">Hobby</TabsTrigger>
         </TabsList>
-        <div className="mt-2 rounded-md border p-4">
+        <div className="flex flex-col mt-2 p-4">
           <TabsContent value="all">
-            <Hardware hardwares={hardware} />
+            <Hardware />
+            <Software />
           </TabsContent>
           <TabsContent value="hardware">
-            <Hardware hardwares={hardware} />
+            <Hardware />
           </TabsContent>
           <TabsContent value="software">
-            <Hardware hardwares={hardware} />
-          </TabsContent>
-          <TabsContent value="coding">
-            <Hardware hardwares={hardware} />
-          </TabsContent>
-          <TabsContent value="hobby">
-            <Hardware hardwares={hardware} />
+            <Software />
           </TabsContent>
         </div>
       </Tabs>
+
     </>
   );
 }
