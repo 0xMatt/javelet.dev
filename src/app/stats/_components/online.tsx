b2386c1c0f9d711fadc41f51ee513deeb7c8a5e5
Matthew Javelet <matthew.javelet@gmail.com>
2025-06-29 13:31:01 -0500

    Remove scroll area from users online

--- src/app/stats/_components/online.tsx
+++ src/app/stats/_components/online.tsx
@@ -2,7 +2,6 @@ import { SessionType } from '@/types/session';
 import StatCard from '@/app/stats/_components/stat-card';
 import SectionTitle from '@/components/elements/section-title';
 import { Card, CardContent } from '@/components/ui/card';
-import { ScrollArea } from '@/components/ui/scroll-area';
 import { Clock, FileText, User } from 'lucide-react';
 import { Badge } from '@/components/ui/badge';
 import { DateTime } from 'luxon';
@@ -37,28 +36,26 @@ export default function Sessions({ sessions }: { sessions: SessionType[] }) {
         <div className="col-span-2">
           <Card>
             <CardContent>
-              <ScrollArea className="h-14">
-                <div className="flex w-full flex-wrap gap-4">
-                  {sessions.map((session) => (
-                    <Badge key={session.id}>
-                      <span className="inline-flex gap-2">
-                        <User size={16} />
-                        {getName(session)}
-                      </span>
-                      <span className="ml-1 inline-flex gap-2">
-                        <Link href={session.location} className="inline-flex">
-                          <FileText size={16} />
-                          {session.location}
-                        </Link>
-                      </span>
-                      <span className="ml-1 inline-flex gap-2">
-                        <Clock size={16} />
-                        {getRelativeDate(session.lastClick.toISOString())}
-                      </span>
-                    </Badge>
-                  ))}
-                </div>
-              </ScrollArea>
+              <div className="flex w-full flex-wrap gap-4">
+                {sessions.map((session) => (
+                  <Badge key={session.id}>
+                    <span className="inline-flex gap-2">
+                      <User size={16} />
+                      {getName(session)}
+                    </span>
+                    <span className="ml-1 inline-flex gap-2">
+                      <Link href={session.location} className="inline-flex">
+                        <FileText size={16} />
+                        {session.location}
+                      </Link>
+                    </span>
+                    <span className="ml-1 inline-flex gap-2">
+                      <Clock size={16} />
+                      {getRelativeDate(session.lastClick.toISOString())}
+                    </span>
+                  </Badge>
+                ))}
+              </div>
             </CardContent>
           </Card>
         </div>
