1e8956f739053dc02bd7251fd7cd89a70eb5ca79
Matthew Javelet <matthew.javelet@gmail.com>
2025-07-08 12:39:58 -0500

    Try to fix page speed scores

--- src/app/(home)/_components/posts.tsx
+++ src/app/(home)/_components/posts.tsx
@@ -2,11 +2,26 @@
 
 import { Post } from '@/types/blog';
 import BlogCard from '@/app/blog/_components/blog-card';
+import { fetcher } from '@/lib/fetcher';
+import CardSkeleton from '@/components/elements/card-skeleton';
+import useSWR from 'swr';
 
-export default function Posts({ posts }: { posts: Post[] }) {
+export default function Posts() {
+  const { data } = useSWR(`/api/blog?page=1&per_page=4`, fetcher);
+  if (!data)
+    return (
+      <>
+        <div className="grid auto-rows-min grid-cols-1 gap-4 lg:grid-cols-3">
+          <CardSkeleton className="h-[430px]" />
+          <CardSkeleton className="h-[430px]" />
+          <CardSkeleton className="h-[430px]" />
+        </div>
+      </>
+    );
+
   return (
     <div className="grid auto-rows-min grid-cols-1 gap-4 lg:grid-cols-3">
-      {posts.map((post: Post) => (
+      {data.map((post: Post) => (
         <BlogCard key={post.slug} post={post} />
       ))}
     </div>
